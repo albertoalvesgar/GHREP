7cf16b948ec2ff034caf152c712a66e6464ab59c
Alber <Alber@DESKTOP-T1E4IHA>
2023-12-03 23:15:03 +0100

    Interfaces y modelo

---
+++ Prueba_GIT/src/main/java/interfaces/Miinterfaz.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface Miinterfaz {
+
+	public int metododevuelveentero();
+}
--- Prueba_GIT/src/main/java/principal/Principal.java
+++ Prueba_GIT/src/main/java/principal/Principal.java
@@ -3,8 +3,8 @@ package principal;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
