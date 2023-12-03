1ff2840ebca4e761d535bce6b3caea2dd2a96e8f
Alber <Alber@DESKTOP-T1E4IHA>
2023-12-03 23:18:57 +0100

    Primera semana de trabajo

--- Prueba_GIT/src/main/java/principal/Principal.java
+++ Prueba_GIT/src/main/java/principal/Principal.java
@@ -4,6 +4,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Comienzo de mi programa dificilísimo");
+		
+		System.out.println("Sigo con de mi programa dificilísimo");
+		
+		System.out.println("Añado funcionalidades a mi mi programa dificilísimo");
+		
+		System.out.println("Estoy muy contento con mi programa dificilísimo");
 		
 	}
 
